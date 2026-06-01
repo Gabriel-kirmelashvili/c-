ea246714e0aeca90785d55c5fb18ccc0d817e011
Gabriel-kirmelashvili <gabrielkirmelashvili123@gmail.com>
2026-06-01 22:20:19 +0400

    Add project files.

---
+++ homework2/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace homework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //davaleba 1
+
+            //Console.Write("seiyvane asaki: ");
+            //int age = int.Parse(Console.ReadLine());
+
+            //if (age >= 18)
+            //    Console.WriteLine("gilocavt, xmis micema segizlia");
+            //else
+            //    Console.WriteLine("samwuxarod ver sezlebt xmis micemas");
+
+
+
+            //davaleba 2
+
+
+            //Console.Write("Enter first number: ");
+            //int a = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter second number: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter third number: ");
+            //int c = int.Parse(Console.ReadLine());
+
+            //if (a >= b && a >= c)
+            //    Console.WriteLine("The first number is the maximum.");
+            //else if (b >= a && b >= c)
+            //    Console.WriteLine("The second number is the maximum.");
+            //else
+            //    Console.WriteLine("The third number is the maximum.");
+
+
+
+
+            //davaleba 3
+
+
+            //Console.Write("Enter first number: ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //Console.Write("Enter second number: ");
+            //int y = int.Parse(Console.ReadLine());
+
+            //int result = (x == y) ? (x + y) * 3 : x + y;
+
+            //Console.WriteLine(result);
+
+
+        }
+    }
+}
