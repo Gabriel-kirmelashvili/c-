46f28eabcee380f6d34e97078080c9b025809169
Gabriel-kirmelashvili <gabrielkirmelashvili123@gmail.com>
2026-06-05 22:25:12 +0400

    davaleba 3

--- home.3/Program.cs
+++ home.3/Program.cs
@@ -4,7 +4,99 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+
+
+
+            //Davaleba 1 
+
+            //Console.WriteLine("Seiyvane username:");
+            //string username = Console.ReadLine();
+
+            //Console.WriteLine("Seiyvane password:");
+            //string password = Console.ReadLine();
+
+            //if (username == "admin" && password == "1234")
+            //{
+            //    Console.WriteLine("Welcome!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Access denied");
+            //}
+
+
+
+
+
+            //Davaleba 2 
+
+            //Console.WriteLine("Seiyvane pirveli ricxvi:");
+            //string input1 = Console.ReadLine();
+            //bool valid1 = double.TryParse(input1, out double num1);
+
+            //Console.WriteLine("Seiyvane operatori (+ - * /):");
+            //string op = Console.ReadLine();
+
+            //Console.WriteLine("Seiyvane meore ricxvi:");
+            //string input2 = Console.ReadLine();
+            //bool valid2 = double.TryParse(input2, out double num2);
+
+            //if (valid1 && valid2)
+            //{
+            //    switch (op)
+            //    {
+            //        case "+":
+            //            Console.WriteLine("Shedeги: " + (num1 + num2));
+            //            break;
+            //        case "-":
+            //            Console.WriteLine("Shедеги: " + (num1 - num2));
+            //            break;
+            //        case "*":
+            //            Console.WriteLine("Shedeги: " + (num1 * num2));
+            //            break;
+            //        case "/":
+            //            if (num2 != 0)
+            //                Console.WriteLine("Shedeги: " + (num1 / num2));
+            //            else
+            //                Console.WriteLine("Nulze gaqopva ar sheidzleba!");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Araswoри operatori!");
+            //            break;
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Gtxovt seiyvane swori ricxvebi!");
+            //}
+
+
+
+
+
+            //  Davaleba 3 
+
+            //Console.WriteLine("Seiyvane sheni asaki:");
+            //int age = int.Parse(Console.ReadLine());
+
+            //if (age >= 0 && age <= 12)
+            //{
+            //    Console.WriteLine("bavshvi");
+            //}
+            //else if (age >= 13 && age <= 19)
+            //{
+            //    Console.WriteLine("tinejjeri");
+            //}
+            //else if (age >= 20 && age <= 64)
+            //{
+            //    Console.WriteLine("zrdasruli");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("pensioneri");
+            //}
         }
     }
 }
