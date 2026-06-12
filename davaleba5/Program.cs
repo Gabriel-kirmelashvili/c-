43d211c9116c4f1c9b61e512b31f3c972b07d0dc
Gabriel-kirmelashvili <gabrielkirmelashvili123@gmail.com>
2026-06-12 22:44:56 +0400

    5

---
+++ davaleba5/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace davaleba5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // დავალება 1: 
+
+
+            //int[] array1 = { 1, 2, 3 };
+            //int[] array2 = { 4, 5, 6 };
+
+            //int[] resultArray = new int[array1.Length + array2.Length];
+
+            //for (int i = 0; i < array1.Length; i++)
+            //{
+            //    resultArray[i] = array1[i];
+            //}
+
+            //for (int i = 0; i < array2.Length; i++)
+            //{
+            //    resultArray[array1.Length + i] = array2[i];
+            //}
+
+            //Console.Write("resultArray = [");
+            //for (int i = 0; i < resultArray.Length; i++)
+            //{
+            //    Console.Write(resultArray[i]);
+            //    if (i < resultArray.Length - 1) Console.Write(", ");
+            //}
+            //Console.WriteLine("]");
+
+
+
+
+
+            // დავალება 2
+
+
+            //int[] array = { 3, 5, -4, 8, 11, 1, -1, 6 };
+
+            //Console.Write("Enter targetSum: ");
+            //int targetSum = int.Parse(Console.ReadLine());
+
+            //int[][] resultArray = new int[array.Length][];
+            //int pairCount = 0;
+
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    for (int j = i + 1; j < array.Length; j++)
+            //    {
+            //        if (array[i] + array[j] == targetSum)
+            //        {
+            //            resultArray[pairCount] = [array[i], array[j]]; 
+            //            pairCount++;
+            //        }
+            //    }
+            //}
+
+            //Console.Write("resultArray = [ ");
+            //foreach (var pair in resultArray)
+            //{
+            //    if (pair == null) break;
+            //    Console.Write($"[{pair[0]}, {pair[1]}] ");
+            //}
+            //Console.WriteLine("]");
+
+
+
+        }
+    }
+}
